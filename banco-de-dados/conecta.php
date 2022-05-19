9043eefa2e6ea25c42727ce83c3b895b81b6082e
Eiji <99904315+gotou-eiji@users.noreply.github.com>
2022-05-19 16:46:19 -0300

    Delete banco-de-dados directory

--- banco-de-dados/conecta.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $conn = mysqli_connect("127.0.0.1","root","","pessoa");
-    mysqli_set_charset($conn,"utf8");
-    if(!$conn){
-        echo "Erro de conexão - ".mysqli_connect_error();
-    }
-?>
